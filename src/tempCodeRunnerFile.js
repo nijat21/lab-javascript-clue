eb986da048bf5ee81cc720bbd6296369b7555db7
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2023-10-11 21:53:20 +0100

    done

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+  // const message = {
+    //     console.log(`${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in ${envelope.room.name} `)
+    // }
